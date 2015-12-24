a7210d88b5fbd175b8b4a8b82f7774cad5c30412
n9i5a <leahbelyea@gmail.com>
2015-12-24 10:47:03 -0400

    day 24 less code

--- 24.py
+++ 24.py
@@ -4,22 +4,11 @@ import itertools
 with open('input/24.txt', 'r') as f:
   presents = [int(x.strip('\n')) for x in f.readlines()]
 
-def getMinPresentsInFront(presents, groupWeight):
-  presents.sort(reverse = True)
-  minPresentsInFront = 0
-  presentSum = 0
-  for present in presents:
-    presentSum += present
-    minPresentsInFront += 1
-    if presentSum >= groupWeight:
-      return minPresentsInFront
-  return None
-
-def getPossibleGroups(presents, minPresentsInFront):
+def getPossibleGroups(presents, grouWeight):
   possibleGroups = []
   leastPresents = None
 
-  for i in range (minPresentsInFront, len(presents) + 1):
+  for i in range (1, len(presents) + 1):
     if leastPresents != None: break
     combinations = itertools.combinations(presents, i)
     for combination in combinations:
@@ -36,19 +25,17 @@ print 'Part 1'
 totalWeight = sum(presents)
 groupWeight = totalWeight / 3
 
-minPresentsInFront = getMinPresentsInFront(presents, groupWeight)
-possibleGroups = getPossibleGroups(presents, minPresentsInFront)
+# minPresentsInFront = getMinPresentsInFront(presents, groupWeight)
+possibleGroups = getPossibleGroups(presents, groupWeight)
 possibleGroups = sorted(possibleGroups, key=lambda group: reduce(operator.mul, group, 1))
 print reduce(operator.mul, possibleGroups[0], 1)
 
-# Part 2
+# # Part 2
 print '\nPart 2'
 
 totalWeight = sum(presents)
 groupWeight = totalWeight / 4
 
-
-minPresentsInFront = getMinPresentsInFront(presents, groupWeight)
-possibleGroups = getPossibleGroups(presents, minPresentsInFront)
+possibleGroups = getPossibleGroups(presents, groupWeight)
 possibleGroups = sorted(possibleGroups, key=lambda group: reduce(operator.mul, group, 1))
 print reduce(operator.mul, possibleGroups[0], 1)
